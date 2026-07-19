3d95a7a8433f1e93036f9da2dafe8494da21a5af
Officer D0ofy <306140362+d0ofy@users.noreply.github.com>
2026-07-18 23:15:44 -0500

    add v1

---
+++ qbittorrent-lftp-upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#############################################################################
+#                             qbittorrent-lftp                              #
+#                               Coder: d0ofy                                #
+#############################################################################
+#                      * Requires qbittorrent & lftp *                      #
+#############################################################################
+# Changelog
+# v1 - Hardcoded remote folders
+
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+torrentcategory=$4
+torrentsavepath=$5
+
+/usr/bin/lftp <bookmark name> -e "mirror -R "$5"/"$2" "/iSO/LiNUX/"; bye" >> /home/scripts/iso-linux_logfile.log 2>&1
